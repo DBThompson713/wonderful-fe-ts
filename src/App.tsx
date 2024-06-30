0e1cce70c22500fdbf00f007c4d4ca57530c87b6
DB Thompson <dbthompson.au@gmail.com>
2024-06-30 17:01:48 +1000

    completed todos now displaying at bottom

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import { getTodos, deleteTodo, addTodo, updateTodo } from './api/todos';
+import { getTodos, deleteTodo, addTodo } from './api/todos';
 import TodoCard from './components/TodoCard';
 import NewTodo from './components/NewTodo';
 import ActionBar from './components/ActionBar';
@@ -20,24 +20,20 @@ function App() {
   const [editingTodoId, setEditingTodoId] = useState<number | null>(null);
 
   useEffect(() => {
-    getTodos().then((data) => {
-      const sortedTodos = data.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+    const fetchData = async () => {
+      const data = await getTodos();
+      const sortedTodos = data.sort((a, b) => {
+        if (a.completed !== b.completed) {
+          return a.completed ? 1 : -1; 
+        }
+        return new Date(a.date).getTime() - new Date(b.date).getTime();
+      });
       setTodos(sortedTodos);
       setFilteredTodos(sortedTodos);
-    });
+    };
+
+    fetchData();
   }, []);
-
-  useEffect(() => {
-    if (editingTodoId !== null) {
-      setShowAddTodo(false);
-    }
-  }, [editingTodoId]);
-
-  useEffect(() => {
-    if (showAddTodo) {
-      setEditingTodoId(null);
-    }
-  }, [showAddTodo]);
 
   const handleSearch = (searchTerm: string) => {
     const filtered = todos.filter((todo) =>
@@ -48,6 +44,10 @@ function App() {
 
   const handleSort = (sortBy: string) => {
     const sorted = [...filteredTodos].sort((a, b) => {
+      if (a.completed !== b.completed) {
+        return a.completed ? 1 : -1; 
+      }
+
       if (sortBy === 'dateAsc') {
         return new Date(a.date).getTime() - new Date(b.date).getTime();
       } else if (sortBy === 'dateDesc') {
@@ -55,27 +55,34 @@ function App() {
       } else if (sortBy === 'alphabetical') {
         return a.todo.localeCompare(b.todo);
       }
+
       return 0;
     });
+
     setFilteredTodos(sorted);
   };
 
   const handleFilter = (filterBy: string) => {
-    let filtered;
+    let filtered: Todo[] = [...todos];
+
     if (filterBy === 'completed') {
       filtered = todos.filter((todo) => todo.completed);
     } else if (filterBy === 'overdue') {
       filtered = todos.filter((todo) => !todo.completed && new Date(todo.date) < new Date());
-    } else {
-      filtered = todos;
     }
+
     setFilteredTodos(filtered);
   };
 
   const handleAddTodo = async (newTodo: Todo) => {
     try {
       const updatedTodos = await addTodo(newTodo);
-      const sortedTodos = updatedTodos.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+      const sortedTodos = updatedTodos.sort((a, b) => {
+        if (a.completed !== b.completed) {
+          return a.completed ? 1 : -1; 
+        }
+        return new Date(a.date).getTime() - new Date(b.date).getTime();
+      });
       setTodos(sortedTodos);
       setFilteredTodos(sortedTodos);
       setShowAddTodo(false);
