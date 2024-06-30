90e3347780f9d50b1bbb70bf0998a2fab2870c8a
DB Thompson <dbthompson.au@gmail.com>
2024-06-30 19:34:35 +1000

    import interface to newTodo from api

--- src/components/NewTodo/index.tsx
+++ src/components/NewTodo/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import './styles.css';
-import { Todo } from './../../App';
+import { Todo } from './../../api/todos';
 
 interface NewTodoProps {
   onAddTodo: (newTodo: Todo) => void;
