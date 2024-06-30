a3332276296122eba8f19f6c6d10a79f399b7b15
DB Thompson <dbthompson.au@gmail.com>
2024-06-30 19:37:30 +1000

    interface imported to progress bar from api

--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.css';
-import { Todo } from './../../App'; 
+import { Todo } from './../../api/todos'; 
 
 interface ProgressBarProps {
   todos: Todo[];
